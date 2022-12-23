b36820dc95a27129c39929d68566a81a845836b7
afimbres <afimbres@nmetric.com>
2022-12-22 21:25:14 -0800

    Solutions pushed

--- 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,28 +1,29 @@
 // TODO: What does 'this' refer to?
+//
 console.log(this);
 
 // TODO: What does 'this' refer to?
-function helloThis() { 
-   console.log("Inside this function, this is " + this); 
-   }
+function helloThis() {
+  console.log("Inside this function, this is " + this);
+}
 
-// TODO: What will this log? 
-var child = { 
-   age: 10,
-   ageTenYears: function() {
-       console.log(this.age + 10); 
-   }
+// TODO: What will this log?
+var child = {
+  age: 10,
+  ageTenYears: function () {
+    console.log(this.age + 10);
+  },
 };
 
-// TODO: What will this log? 
-var investor = {  
+// TODO: What will this log?
+var investor = {
   name: "Cash Saver",
   investment: {
     initialInvestment: 5000,
-    investmentGrowth: function() {
-      console.log(this.initialInvestment * 1.15)
-    }
-   }   
+    investmentGrowth: function () {
+      console.log(this);
+    },
+  },
 };
 
 // TODO: After commenting, check your results!
