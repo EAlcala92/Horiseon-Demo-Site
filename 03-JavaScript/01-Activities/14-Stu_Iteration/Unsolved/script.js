b2979b376a19b5660635097d01602f8b805a2cbd
EAlcala92 <119818076+EAlcala92@users.noreply.github.com>
2022-12-28 20:39:35 -0800

    Delete 03-JavaScript directory

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++
@@ -1,6 +0,0 @@
-//WRITE YOUR CODE BELOW
-var names= ["Bruce","Clark","Diana","Oliver","Todd", "Jason"];
-
-for(var i=0; i < names.length; i++){
-    console.log("Great to see you " + names[i]);--
-}
